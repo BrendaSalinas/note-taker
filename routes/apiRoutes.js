57030c56adb3afdb56acdd3674767921406de3c7
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-01-31 10:18:28 -0600

    API get request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,17 @@
 const router = require('express').Router();
 
-router.post('/notes', (req,res)=>{
-    
+const readFiles = util.promisify(fs.readFile);
+const writeFiles = util.promisify(fs.writeFile);
+
+router.get('/notes', function(req, res) {
+    readFiles('./db/db.json', "utf8").then(function(inputs) {
+        note = [].concat(JSON.parse(inputs))
+        res.json(note);
+    });
+});
+
+router.post('/notes', function(req, res) {
+    co
 })
+
+module.exports = router;
